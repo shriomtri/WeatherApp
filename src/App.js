3a5cbea31a26ee803d9d19835b847d6f46f80735
Shriom <shriomtripathi33@gmail.com>
2018-07-06 01:33:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const baseURL = "https://api.github.com/users/";
+
+
+class App extends Component{
+
+  render(){
+
+    return (
+      <div className="container">
+          <h1>Hello,World!</h1>
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
