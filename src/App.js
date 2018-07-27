066243096b746ec2cee173fdd64569b551c2306a
Shriom <shriomtripathi33@gmail.com>
2018-07-27 10:49:22 +0530

    Done passing value from input textbox to parent component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 const baseURL = "https://api.github.com/users/";
 
+//Title
 function Title() {
-  return(
-    <div>
-      <img className="gifImg" src="https://octodex.github.com/images/maxtocat.gif"/>
-      <div className="title">
-        <h3>GitHub</h3>
-        <h5>Find out your self on github.</h5>
-      </div>
-    </div>
-  );
-  
+    return (
+        <div className="container">
+            <img className="gifImg" src="https://media.giphy.com/media/12zV7u6Bh0vHpu/giphy.gif"/>
+            <div className="title">
+                <h2> GitHub</h2>
+                <h5>Find out your self on github.</h5>
+            </div>
+        </div>
+    );
+
+}
+
+//Search
+class Search extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" className="search input-lg" placeholder="Username" onChange={this.props.onChange}/>
+            </div>
+        )
+    }
 }
 
 
-class App extends Component{
+class App extends Component {
 
-  render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: ""
+        };
 
-    return (
-      <div className="container main card">
-        <div className="row">
-          <div className="col-md-6 titleColumn">
-            <Title />
-          </div>
-          <div className="col-md-6 searchColumn"></div>
-        </div>
-      </div>
-    );
+        this.handleOnChange = this.handleOnChange.bind(this)
 
-  }
+    }
+
+    //getTheUserHandle
+    handleOnChange(event) {
+
+        console.log("I am here");
+        let userHandle = event.target.value;
+
+        this.setState({username: userHandle});
+        console.log(userHandle);
+
+    }
+
+    render() {
+
+        return (
+            <div className="container main card">
+                <div className="row">
+                    <div className="col-md-6 titleColumn">
+                        <Title/>
+                    </div>
+
+                    <div className="col-md-6 searchColumn">
+
+                        <Search onChange={this.handleOnChange}/>
+
+                        <h2>{this.state.username}</h2>
+
+                    </div>
+
+                </div>
+            </div>
+        );
+
+    }
 
 }
 
